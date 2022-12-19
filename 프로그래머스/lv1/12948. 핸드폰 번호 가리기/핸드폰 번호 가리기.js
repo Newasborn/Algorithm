1159f25940249cb95d63e9d3d01002c19970d452
Newasborn <99702761+Newasborn@users.noreply.github.com>
2022-12-19 11:35:57 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
+++ 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -1,9 +1,4 @@
 function solution(phone_number) {
-    let answer = ''
-    let arr = phone_number.split('').splice(-4, 4);
-    let star = ''
-    for(let i = 0; i < phone_number.length-4; i++){
-    star += '*'}
-    answer = star + arr.join('')
+    let answer = '*'.repeat(phone_number.length - 4) + phone_number.slice(-4);
     return answer;
 }
