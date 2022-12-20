a36c4d0e051fb9ad84a0aab176db4483812169ac
Newasborn <99702761+Newasborn@users.noreply.github.com>
2022-12-20 11:38:06 +0900

    [level 0] Title: 암호 해독, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120892. 암호 해독/암호 해독.js
@@ -0,0 +1,8 @@
+  function solution(cipher, code) {
+    let answer = '';
+
+    for (let i = 1; i * code <= cipher.length; i++) {
+      answer += cipher[code * i - 1];
+    }
+    return answer;
+  }
