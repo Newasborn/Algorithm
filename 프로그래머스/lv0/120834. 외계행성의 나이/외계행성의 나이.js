0732d15f4e78bbc286c1746ee2ffdfa4121746d2
Hoesu <99702761+Newasborn@users.noreply.github.com>
2023-02-02 11:20:02 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.05 ms, Memory: 33 MB -BaekjoonHub

--- 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.js
+++ 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.js
@@ -10,22 +10,7 @@ let alphabetArr = [
     'h',
     'i',
     'j',
-    'k',
-    'l',
-    'm',
-    'n',
-    'o',
-    'p',
-    'q',
-    'r',
-    's',
-    't',
-    'u',
-    'v',
-    'w',
-    'x',
-    'y',
-    'z',
+
   ];
   let ageArr = String(age).split('');
   let answer=[];
