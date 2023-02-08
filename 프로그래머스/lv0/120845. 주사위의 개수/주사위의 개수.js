68b1535c112e26c7f072777cc06324f57f44c605
Hoesu <99702761+Newasborn@users.noreply.github.com>
2023-02-08 23:36:03 +0900

    [level 0] Title: 주사위의 개수, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120845. 주사위의 개수/주사위의 개수.js
+++ 프로그래머스/lv0/120845. 주사위의 개수/주사위의 개수.js
@@ -2,7 +2,7 @@
     let answer = 0;
 
     answer = box
-      .map((v) => Math.floor(Number(v) / n))
+      .map((v) => Math.floor(v / n))
       .reduce((prev, curr) => (prev *= curr));
 
     return answer;
