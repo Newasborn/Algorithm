003c39574601f37c8e19d9dde972fadc46e7f64a
Hoesu <99702761+Newasborn@users.noreply.github.com>
2023-02-10 00:55:23 +0900

    [level 0] Title: 팩토리얼, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120848. 팩토리얼/팩토리얼.js
+++ 프로그래머스/lv0/120848. 팩토리얼/팩토리얼.js
@@ -1,17 +1,11 @@
-function solution(n) {
-    let answer = 0;
-    let save = 1; 
-    
-    for(let i = 1; i <= 10; i++){
-        save = save * i
-        if(save == n){
-            answer = i
-            return answer
-        } else if (save > n){
-            answer = i -1
-            return answer
-        }
-        
+  function solution(n) {
+    let save = 1;
+    for (let i = 1; i <= 10; i++) {
+      save = save * i;
+      if (save == n) {
+        return i;
+      } else if (save > n) {
+        return i - 1;
+      }
     }
-    
-}
+  }
